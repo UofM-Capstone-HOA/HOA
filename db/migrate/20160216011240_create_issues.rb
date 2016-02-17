3f96b547edbe9b6f35be6027c8335f2ac8feeacc
Josh Restuccio <jrestuccio@github.com>
2016-02-16 18:27:37 -0600

    removed primary key id columns

--- db/migrate/20160216011240_create_issues.rb
+++ db/migrate/20160216011240_create_issues.rb
@@ -1,14 +1,13 @@
 class CreateIssues < ActiveRecord::Migration
   def change
     create_table :issues do |t|
-      #t.primary_key :id
       t.datetime :date
       t.references :address, index: true, foreign_key: true
       t.references :homeowner, index: true, foreign_key: true
       t.references :letter, index: true, foreign_key: true
       t.boolean :lien
       t.boolean :resolved
-      #t.references :user, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.text :note
       t.string :picture
       t.references :issueCategory, index: true, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160216012932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "addresses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "home_owners", force: :cascade do |t|
+    t.string   "firstName"
+    t.string   "lastName"
+    t.string   "email"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "issue_categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "issues", force: :cascade do |t|
+    t.datetime "date"
+    t.boolean  "lien"
+    t.boolean  "resolved"
+    t.text     "note"
+    t.string   "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "letters", force: :cascade do |t|
+    t.integer  "issue_id"
+    t.integer  "letterNum"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "letters", ["issue_id"], name: "index_letters_on_issue_id", using: :btree
+
+  add_foreign_key "letters", "issues"
 end
