bc4a49f26fe19d216bbe7ab0e12cbdc8e3334427
Josh Restuccio <jrestuccio@github.com>
2016-02-25 17:25:03 -0600

    added admin page, still needs styling and users list page

--- app/models/home_owner.rb
+++ app/models/home_owner.rb
@@ -1,2 +1,4 @@
 class HomeOwner < ActiveRecord::Base
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     end
   end
 
+
    get 'home', to: 'static_pages#home', as: 'home'
 
    resources :home_owners
