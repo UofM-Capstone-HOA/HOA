ced229e911393b98e03254d296eb7f684fa6dbbd
Kevin Townsend <kevinatown@gmail.com>
2016-03-13 21:44:31 +0000

    testing show modals

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,6 @@
 class AdminController < ApplicationController
 	before_action :authenticate_user!
   def show
-  
   	@home_owners = HomeOwner.all
   	@addresses = Address.all
   	@issue_categories = IssueCategory.all
@@ -9,7 +8,13 @@ class AdminController < ApplicationController
   	@home_owner = HomeOwner.new
   	@address = Address.new
     @issue_category = IssueCategory.new
-  
+  	
+  end
+
+  def show_ic
+ 	show
+  	@issue_category_show = IssueCategory.find(params[:issue_category_show])
+  	render :show, id: current_user 
   end
 
 
