09e6fb99b9598cbd70922fe86422c6953c364f25
Kevin Townsend <kevinatown@gmail.com>
2016-03-26 21:16:40 +0000

    map sizing

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -7,6 +7,35 @@ class IssuesController < ApplicationController
   # GET /issues.json
   def index
     @issues = Issue.all
+    @hash = Gmaps4rails.build_markers(@issues) do |issue, marker|
+      marker.lat issue.address.latitude
+      marker.lng issue.address.longitude
+      marker.infowindow issue.address.full_address + "<br />" + issue.note +
+      "<br />" + "Status: " + issue.issue_status
+
+
+
+      if issue.lien == false && issue.resolved == false
+          marker.picture({
+        :url => "/assets/IssueOpen.gif",
+        :width => 32,
+        :height => 32
+        })
+      elsif issue.resolved == true
+          marker.picture({
+        :url => "/assets/IssueResolved.gif",
+        :width  => 32,
+        :height => 27
+        })
+      else
+          marker.picture({
+        :url => "/assets/IssueLien.gif",
+        :width  => 32,
+        :height => 32
+        })
+      end
+    end
+ 
   end
 
   # GET /issues/1
